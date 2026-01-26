264c59a62deaad3b6cd16e9ad99cd4f3de9eae46
hec <hec.dev@pm.me>
2026-01-25 19:45:48 -0800

    GMA-13: Implement Material3 theme system with dark/light support
    - Create Color.kt with brand colors (BrightRed, DarkNavy, CharcoalGray)
    - Create ColorScheme.kt with LightColorScheme and DarkColorScheme
      - Light: White background, Black text
      - Dark: CharcoalGray background, White text
    - Create Shape.kt with Material3 standard shapes (4dp to 28dp)
    - Create Type.kt with Material3 typography scale
    - Create Theme.kt with GMATheme composable
    - Support cross-platform dark mode detection (Android, iOS, Desktop, Web)
    - All colors, shapes, and typography now accessed via MaterialTheme.*

---
+++ composeApp/src/commonMain/kotlin/com/gma/tsunjo/school/theme/Color.kt
@@ -0,0 +1,80 @@
+package com.gma.tsunjo.school.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Primary Colors
+val DarkNavy = Color(0xFF1C2340)
+val DeepRed = Color(0xFF8B1A1A)
+val BrightRed = Color(0xFFC41E3A)
+val DarkRed = Color(0xFF6B0F1A)
+
+// Neutral Colors
+val OffWhite = Color(0xFFF5F5F5)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+val LightGray = Color(0xFFE0E0E0)
+val MediumGray = Color(0xFF9E9E9E)
+val DarkGray = Color(0xFF424242)
+val CharcoalGray = Color(0xFF2B2B2B)
+
+// Light Theme Colors
+val LightPrimary = BrightRed
+val LightOnPrimary = White
+val LightPrimaryContainer = Color(0xFFFFDAD6)
+val LightOnPrimaryContainer = DarkRed
+
+val LightSecondary = DarkNavy
+val LightOnSecondary = White
+val LightSecondaryContainer = Color(0xFFD6E3FF)
+val LightOnSecondaryContainer = Color(0xFF0D1B3E)
+
+val LightTertiary = DeepRed
+val LightOnTertiary = White
+val LightTertiaryContainer = Color(0xFFFFD9E2)
+val LightOnTertiaryContainer = Color(0xFF3E0016)
+
+val LightError = Color(0xFFBA1A1A)
+val LightOnError = White
+val LightErrorContainer = Color(0xFFFFDAD6)
+val LightOnErrorContainer = Color(0xFF410002)
+
+val LightBackground = White
+val LightOnBackground = Black
+val LightSurface = OffWhite
+val LightOnSurface = Black
+val LightSurfaceVariant = Color(0xFFE7E0EC)
+val LightOnSurfaceVariant = Color(0xFF49454F)
+
+val LightOutline = Color(0xFF79747E)
+val LightOutlineVariant = Color(0xFFCAC4D0)
+
+// Dark Theme Colors
+val DarkPrimary = BrightRed
+val DarkOnPrimary = White
+val DarkPrimaryContainer = DeepRed
+val DarkOnPrimaryContainer = White
+
+val DarkSecondary = Color(0xFFADC6FF)
+val DarkOnSecondary = Color(0xFF0A2F66)
+val DarkSecondaryContainer = DarkNavy
+val DarkOnSecondaryContainer = Color(0xFFD6E3FF)
+
+val DarkTertiary = Color(0xFFFFB0C8)
+val DarkOnTertiary = Color(0xFF5E1129)
+val DarkTertiaryContainer = DarkRed
+val DarkOnTertiaryContainer = Color(0xFFFFD9E2)
+
+val DarkError = Color(0xFFFFB4AB)
+val DarkOnError = Color(0xFF690005)
+val DarkErrorContainer = Color(0xFF93000A)
+val DarkOnErrorContainer = Color(0xFFFFDAD6)
+
+val DarkBackground = CharcoalGray
+val DarkOnBackground = White
+val DarkSurface = DarkNavy
+val DarkOnSurface = White
+val DarkSurfaceVariant = Color(0xFF49454F)
+val DarkOnSurfaceVariant = Color(0xFFCAC4D0)
+
+val DarkOutline = Color(0xFF938F99)
+val DarkOutlineVariant = Color(0xFF49454F)
---
+++ composeApp/src/commonMain/kotlin/com/gma/tsunjo/school/theme/Theme.kt
@@ -0,0 +1,20 @@
+package com.gma.tsunjo.school.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun GMATheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        shapes = AppShapes,
+        content = content
+    )
+}
